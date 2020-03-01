4dbcd2b2cea26612b71d2bb4d0138c6d98323f64
wangwen7 <871688592@qq.com>
2020-03-01 16:24:35 +0800

    应用服务更改

---
+++ consensus-services/consensus-biz/consensus-apply-service/src/main/java/com/shzz/consesus/apply/servlce/ApplyConsensus.java
@@ -0,0 +1,14 @@
+package com.shzz.consesus.apply.servlce;
+
+/**
+ * Copyright (C) 2018 HangZhou Hikvision System Technology Co., Ltd. All Right Reserved.
+ * Description：http://www.hikvision.com
+ *
+ * @Classname ApplyConsensus
+ * @Description TODO
+ * @Date 2020/3/1 16:23
+ * @Created by wangwen7
+ */
+
+public class ApplyConsensus {
+}
