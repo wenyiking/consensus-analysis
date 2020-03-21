6bbb0fc616f9546cebca8715d6430157eb716f20
wangwen7 <871688592@qq.com>
2020-03-21 14:18:30 +0800

    api层

---
+++ consensus-services/consensus-api/src/main/java/com/shzz/consensus/apis/Interf.java
@@ -0,0 +1,10 @@
+package com.shzz.consensus.apis;
+
+/**
+ * @Classname Interf
+ * @Description TODO
+ * @Date 2020/3/21 13:20
+ * @Created by wangwen7
+ */
+public interface Interf {
+}
---
+++ consensus-services/consensus-api/src/main/java/com/shzz/consensus/apis/data/crawler/NetDataCrawler.java
@@ -0,0 +1,10 @@
+package com.shzz.consensus.apis.data.crawler;
+
+/**
+ * @Classname NetDataCrawler
+ * @Description TODO
+ * @Date 2020/3/21 13:22
+ * @Created by wangwen7
+ */
+public interface NetDataCrawler {
+}
